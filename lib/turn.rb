bb49fd42b37591b3fefab5a92fbc39c5af251d5f
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 19:10:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,13 +28,20 @@ def valid_move?(board, index)
   end
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-end
-  
+
 def move(board, index, character = "X")
   board[index] = character
   return board
+end
+
+def turn (board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index) == true
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
