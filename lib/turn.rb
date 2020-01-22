cd271d0cf1234442564f2333623bf25655ea1871
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 17:34:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,13 @@ end
 def input_to_index(int)
   index = (int.to_i) -1 
 end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    puts "this is a valid move"
+    return true 
+  else 
+    return false 
+  end
+end
+  
