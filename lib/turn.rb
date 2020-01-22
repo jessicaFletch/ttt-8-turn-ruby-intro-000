7b0bba4c85b235cec90a0cf519801814e78ed204
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 16:59:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,13 @@ def input_to_index(move)
 end
 
 
+if valid_move?(board,index)
+    puts 'valid move'
+    move(board, index, token)
+    display_board(board)
+   else
+    puts 'try again'
+    turn(board)
+  end
+  display_board(board)
+end
