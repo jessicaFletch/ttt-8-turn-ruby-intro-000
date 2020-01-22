3510287520562908503e39cc674c8805244c3ca1
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 17:40:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,8 +16,7 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
-    puts "this is a valid move"
+  if index.between?(0,8)
     return true 
   else 
     return false 
