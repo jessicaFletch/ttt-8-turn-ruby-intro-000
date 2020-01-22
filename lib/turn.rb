59a98f49b5395c3a2c603f9eac40761542f367fc
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 18:15:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,14 @@ def turn(board)
   index = input_to_index(user_input)
 end
   
-def move(board)
+def current_player(board)
+  if turn_count(board)%2 ==0
+    current_player = "X"
+  else
+    current_player = "O"
+end
+return current_player
+end
     
    
 def move (board)
