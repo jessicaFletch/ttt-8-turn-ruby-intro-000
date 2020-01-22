bf2cf47fcd3368401839c274f09fda8062b11ef9
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 17:59:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,7 +29,7 @@ end
 def turn(board)
   puts "please enter 1-9:"
   user_input = gets.strip
-  index = inout_to_index(user_input)
+  index = input_to_index(user_input)
   token = current_player(board)
 end
   
