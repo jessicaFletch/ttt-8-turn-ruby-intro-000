787853159007fba18726352d8bf1a66b11c837f7
Jessica Fletcher <jessica.dolata@gmail.com>
2020-01-22 17:47:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,9 +14,12 @@ def input_to_index(int)
   index = (int.to_i) -1 
 end
 
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
 
 def valid_move?(board, index)
-  if index.between?(0,8) && !
+  if index.between?(0,8) && !position_taken?(board, index)
     return true   
   else 
     return false
